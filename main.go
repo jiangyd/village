c27fbedcec5ff778ce21be749922061fe26248b1
江永道 <jiangyd@jiangyd.local>
2017-03-03 12:41:42 +0800

    commit

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/astaxie/beego/session/mysql"
 	_ "github.com/go-sql-driver/mysql"
-	"tester/models"
-	_ "tester/routers"
+	"village/models"
+	_ "village/routers"
 )
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/tester?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(192.168.56.102:3377)/village?charset=utf8")
 	orm.RegisterModel(
 		new(models.User),
 		new(models.Topic),
@@ -25,7 +25,7 @@ func main() {
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.BConfig.WebConfig.Session.SessionName = "SessionId"
 	beego.BConfig.WebConfig.Session.SessionProvider = "mysql"
-	beego.BConfig.WebConfig.Session.SessionProviderConfig = "root:123456@tcp(127.0.0.1:3306)/tester?charset=utf8"
+	beego.BConfig.WebConfig.Session.SessionProviderConfig = "root:123456@tcp(192.168.56.102:3377)/village?charset=utf8"
 	/*end*/
 	beego.Run()
 
