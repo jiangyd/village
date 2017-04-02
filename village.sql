0d14ad2aeafdb42704e5eaef18e32dfbeb5cbc45
jiangyd <962584902@qq.com>
2017-04-02 18:19:14 +0800

    update

--- village.sql
+++ village.sql
@@ -2,3 +2,11 @@
 ALTER TABLE dz ADD UNIQUE KEY (type,type_id,uid_id);
 ALTER TABLE collection ADD UNIQUE KEY (type,type_id,uid_id);
 ALTER TABLE firend ADD UNIQUE KEY (user_a_id,user_b_id);
+
+--创建session表
+CREATE TABLE `session` (
+	`session_key` char(64) NOT NULL,
+	`session_data` blob,
+	`session_expiry` int(11) unsigned NOT NULL,
+	PRIMARY KEY (`session_key`)
+	) ENGINE=MyISAM DEFAULT CHARSET=utf8;
