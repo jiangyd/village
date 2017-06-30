a95b68725c8a2dc68cb61a2e645efd905df2546a
江永道 <jiangyd@jiangyd.local>
2017-06-30 17:31:04 +0800

    test

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -372,9 +372,6 @@ func (self *UserController) Message() {
 		self.Ctx.Redirect(302, "/")
 	} else {
 		msgdata := models.GetMyMsg(&models.User{Id: uid.(int)}, msgtype)
-		for _, a := range msgdata {
-			fmt.Println(a.Content)
-		}
 		self.Data["json"] = &msgdata
 		self.ServeJSON()
 	}
